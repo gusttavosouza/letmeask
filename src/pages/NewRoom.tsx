b0f8ad94965f415a1a5ea26ceacdfa8ba63cc189
gusttavosouza <gustavorobertotb@outlook.com>
2021-06-26 19:01:30 -0300

    :construction: Feat: Add user authentication context

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/UseAuth';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/auth.scss'
+
+export const NewRoom: React.FC = () => {
+
+  // const { user } = useAuth();
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>Quer entrar em um sala existente? <Link to="/">clique aqui</Link></p>
+        </div>
+      </main>
+    </div>
+  );
+}
